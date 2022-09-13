5d825a9fbe04ff21f1af25b3c64198349632522d
Marcelo Lynx <marcelolynx@gmail.com>
2022-09-12 20:59:43 -0400

    Add files via upload

---
+++ IValidation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UGDAT.Business.Interfaces
+{
+    public interface IValidation
+    {
+      
+        Task<string> Validar(string file);
+       
+    }
+}
