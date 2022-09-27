a1e682d917d789105e28e345c2438cee99c3dbea
Marcelo Lynx <marcelolynx@gmail.com>
2022-09-27 13:20:01 -0400

    Abertura Validation
    classe para validacao dos atributos do model abertura.

---
+++ AberturaValidation.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+using UGDAT.Business.Models;
+using UGDAT.Business.CustonValid;
+
+namespace UGDAT.Business.Validations
+{
+    public class AberturaValidation : AbstractValidator<Abertura>
+    {
+       
+
+        public AberturaValidation(string line)
+        {
+           
+            RuleFor(res => res.Registro)
+               .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+               .NotNull().WithMessage("O campo {PropertyName} nao pode ser nulo");
+
+            RuleFor(res => res.Versao)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+                .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+             RuleFor(res => res.CodFinalidade)
+               .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+               .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+             RuleFor(res => res.IeSolicitante)
+              .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+              .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido"); 
+
+            RuleFor(res => res.CnpjSolicitante)
+              .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+              .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido")
+              .Must(vl => vl.IsValidCnpj());
+
+            RuleFor(res => res.RegimeTribultario)
+             .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+             .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+            RuleFor(res => res.DtInicioPeriodo)
+            .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido")
+            .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido");
+
+            RuleFor(res => res.Ambiente)
+             .NotEmpty().WithMessage("O campo {PropertyName} Nao pode ser preenchido")
+             .NotNull().WithMessage("O campo {PropertyName} precisa ser fornecido");
+        }
+
+        
+    }
+}
